3f7c8fa8f9cd70e90941860c687ab76b9b7ac685
Raxit28 <raxitptl28@gmail.com>
2024-09-16 12:49:53 -0400

    initial commit

---
+++ src/firstapplication/Firstapplication.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package firstapplication;
+
+/**
+ *
+ * @author raxit
+ */
+public class Firstapplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    //added a comment
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello");
+    }
+    
+}
