6dc682f0863a4dee5a0265bec5b05f45d8f7483f
Raxit28 <raxitptl28@gmail.com>
2024-11-03 23:28:54 -0500

    final commit

--- src/firstapplication/Firstapplication.java
+++ src/firstapplication/Firstapplication.java
@@ -20,7 +20,7 @@ public class Firstapplication {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello");
-        System.out.println("Raxit");
+        System.out.println("Raxitnnn");
     }
     
 }
